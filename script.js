e08bb162c70bfdb250f3891c469b524f5a69586e
grmonteiro <gmonteiro@lekooa.com>
2018-02-05 20:52:58 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkValidation(page) {
+    if (page == "index") {
+        if (localStorage.getItem("isValidated") !== null) {
+            location.replace("accueil.html");            
+        }
+    } else {
+        if (localStorage.getItem("isValidated") === null) {
+            location.replace("index.html");            
+        }
+    }
+}
+
+function ageVerification(validation) {
+    if (validation == "ok") {
+        localStorage.setItem("isValidated", validation);
+        location.replace("accueil.html");
+    } else {
+        location.href = "https://google.fr";
+    }
+}
